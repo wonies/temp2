7898eb7c4fcb9b8cccb315aa1be186202938015a
wonies <bluewoniee@gmail.com>
2023-05-19 19:23:06 +0900

    complete

--- mandatory/ready/main.c
+++ mandatory/ready/main.c
@@ -6,16 +6,11 @@
 /*   By: wonhshin <wonhshin@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 22:05:32 by wonhshin          #+#    #+#             */
-/*   Updated: 2023/05/18 22:05:59 by wonhshin         ###   ########.fr       */
+/*   Updated: 2023/05/19 19:22:55 by wonhshin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
-
-// void leaks()
-// {
-// 	system("leaks push_swap");
-// }
 
 int	main(int ac, char **av)
 {
@@ -23,7 +18,7 @@ int	main(int ac, char **av)
 	t_node	*b;
 	t_info	info;
 	char	**get;
-	// atexit(leaks);
+
 	a = NULL;
 	b = NULL;
 	get = NULL;
